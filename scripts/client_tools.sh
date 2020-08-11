17f50b7dbe46ca1da7ae577bddec851131ca347e
Steven Fitzpatrick <stevthedev@gmail.com>
2020-08-10 23:36:57 -0500

    Whitespace

--- scripts/client_tools.sh
+++ scripts/client_tools.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Download cloudflare ssl (cfssl) and cfssljson
 # https://github.com/cloudflare/cfssl/releases/
 
@@ -16,14 +15,12 @@ sudo mv cfssljson_1.4.1_linux_amd64 /usr/local/bin/cfssljson
 
 cfssl version
 
-
 # Download kubectl binary
 # https://kubernetes.io/docs/tasks/tools/install-kubectl/
 
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 
 chmod +x ./kubectl
-
 sudo mv ./kubectl /usr/local/bin/kubectl
 
 kubectl version --client
